e8fbe7c656211eee0ec496d798142fbf78f109cd
yipeiguo <17133097+yipeiguo@users.noreply.github.com>
2025-12-06 19:15:59 +0800

    Delete simulationCode directory

--- simulationCode/genlogisticrnd.m
+++
@@ -1,9 +0,0 @@
-% This is a function for drawing from type1 genlogistic distribution
-
-function rnd = genlogisticrnd(a,loc,scale,nsamples)
-
-    u = rand(nsamples,1);
-    xtildeVec = -log(u.^(-1/a)-1);
-    rnd = scale.*xtildeVec + loc;
-
-end
